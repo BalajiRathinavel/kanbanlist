ed53338b76bd08e271de7b2a4814205a4b67db82
BalajiRathinavel <rvlbalaji@gmail.com>
2019-10-17 17:47:07 +0530

    Updates

---
+++ precache-manifest.bbc31573c151a7a21e164c1a08d246d7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9606abb40e050b3497bcc78d5cfe50",
+    "url": "/kanbanlist/index.html"
+  },
+  {
+    "revision": "8618acf42f243c364c28",
+    "url": "/kanbanlist/static/js/2.3729e6f0.chunk.js"
+  },
+  {
+    "revision": "3c790361126fea6de2d9",
+    "url": "/kanbanlist/static/js/main.de971156.chunk.js"
+  },
+  {
+    "revision": "12f783eca60857f0a95e",
+    "url": "/kanbanlist/static/js/runtime-main.a2fd8514.js"
+  }
+]);
